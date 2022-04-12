85114bdd9533b405596e859bd762187cfeb3f36f
hamza <hamza@jsfactory.co>
2022-04-12 17:30:22 +0000

    render by global id

---
+++ themes/index.js
@@ -0,0 +1,14 @@
+const t1 = {
+  primary: "#570DF8",
+  secondary: "#F000B8",
+  accent: "#37CDBE",
+  neutral: "#3D4451",
+  info: "#3ABFF8",
+  success: "#36D399",
+  warning: "#FBBD23",
+  error: "#F87272",
+};
+
+module.exports = {
+  t1,
+};
